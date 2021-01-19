4c6801681b48eaff9b7f59325d5be490dd0b4e47
AlDrean <lucas.henrique.waki@gmail.com>
2021-01-18 22:26:12 -0300

    RED4 - added test for recentlist a and b - ordering and reopenning

--- test_u6.py
+++ test_u6.py
@@ -14,6 +14,7 @@ class Program:
         self.selectFile = -1
         self.file_content = ""
         self.file_recents = []
+        self.file_recents_num = []
 
 
     def list_files(self):        
@@ -27,15 +28,26 @@ class Program:
         f = open(self.directory+self.onlyfiles[self.selectFile], "r")
         self.file_content = f.read()
         f.close()
+
         return self.file_content
     
 
     def recentList(self):
         return self.file_recents
 
-    def recentList_add(self):
-        pass
+    def recentList_num(self):
+        return self.file_recents_num
+    
+    def recentList_add(self,num,name):
+        if name in self.file_recents:
+            self.file_recents.remove(name)
+            self.file_recents.insert(0,name)
 
+            self.file_recents_num.remove(num)
+            self.file_recents_num.insert(0,name)
+        else:
+            self.file_recents.insert(0,name)
+            self.file_recents_num.insert(0,num)
 
 
 
@@ -79,4 +91,27 @@ class Teste:
         expected = []
 
         assert out == expected
+
+    def test_recentList_add_a(self):
+        #esperada saida dos noems dos arquivos com o primeiro primeiro sendo o 5;
+        program =Program(files_directory)
+        program.open_file(5)
+        program.open_file(6)
+
+        expected = [6,5]
+        out = program.recentList_num()
+        assert out == expected
+    
+    def test_recentList_add_b(self):
+        #esperada saida dos noems dos arquivos com o primeiro primeiro sendo o 5;
+        program =Program(files_directory)
+        program.open_file(5)
+        program.open_file(6)
+        program.open_file(5)
+
+        out = program.recentList_num()
+
+        expected = [5,6]
+        assert out == expected
+
 
