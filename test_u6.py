c3dd7b759b6fb3e0078b7ac018762b5e4e10594f
AlDrean <lucas.henrique.waki@gmail.com>
2021-01-18 21:46:47 -0300

    RED1 - listing directory; adicionado arquivo de saida esperada do list_directory

---
+++ test_u6.py
@@ -0,0 +1,43 @@
+#test_capitalize.py 
+
+import collections 
+
+from os import listdir
+from os.path import isfile, join
+import pytest
+
+class Program:
+
+    def __init__(self, directory):
+        self.directory = directory
+
+    def list_files(self):
+        onlyfiles =""
+        print (onlyfiles)
+        return onlyfiles
+
+
+
+
+
+
+files_directory = 'files/'
+program =Program(files_directory)
+program.list_files()
+testCase = ''
+
+class Teste:
+
+    def test_listFiles(self,capsys):
+        f = open ("tests/list_file.out","r")
+        expect = (f.read())
+
+        program.list_files()
+        captured = capsys.readouterr()
+        
+
+        assert (captured.out == expect)
+
+
+
+
---
+++ u6.py
@@ -0,0 +1,19 @@
+import program
+
+class test():
+    def __init__(self,program):
+        self.files_directory = 'files/'
+        self.test_directory = 'test/'
+        self.Program = program
+        self. fcheck =""
+
+    def tTest(self):
+        assert True
+    def fTest(self):
+        assert False
+
+#    def test_listFiles(self):
+        
+        #Class for testing if listing all the files in the default directory;
+ #       assert True
+
