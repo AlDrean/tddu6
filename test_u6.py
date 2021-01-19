adab8f0fe37cf0c0c7eecb5409084242e189d2a7
AlDrean <lucas.henrique.waki@gmail.com>
2021-01-18 22:06:06 -0300

    RED2 openfile test e arquivo de saida esperado adicionados. ADicionada função minima open_file

--- test_u6.py
+++ test_u6.py
@@ -11,19 +11,26 @@ class Program:
     def __init__(self, directory):
         self.directory = directory
         self.onlyfiles = ""
+        self.selectFile = -1
+        self.file_content = ""
 
     def list_files(self):        
         self.onlyfiles = [f for f in listdir(self.directory) if isfile(join(self.directory, f))]
-        print(*self.onlyfiles, sep = "\n") 
-        
-
+        return (self.onlyfiles)
+    
+    def open_file(self,numFile):
+        return self.file_content
+    
+    
 
 
 
 
 files_directory = 'files/'
+
 program =Program(files_directory)
 program.list_files()
+program.open_file(5)
 testCase = ''
 
 class Teste:
@@ -32,12 +39,21 @@ class Teste:
         f = open ("tests/list_file.out","r")
         expect = (f.read())
 
-        program.list_files()
+        x = program.list_files()
+        print(*x, sep = "\n") 
         captured = capsys.readouterr()
         
 
         assert (captured.out == expect)
 
 
+    def test_openFile(self,capsys):
+        f = open ("tests/open_file.out","r")
+        expect = (f.read())
+        
+        selectedfile = 5
+        out = program.open_file(selectedfile)
+        
+        assert out == expect
 
 
